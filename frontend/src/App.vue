645471c14368d6e22d909a184d86dd4008915e96
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-05 20:49:29 -0300

    chore: add montserrat

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,11 +13,17 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+#app,
+html,
+body,
+input,
+button,
+select {
+  font-family: 'Montserrat', sans-serif;
 }
 </style>
