c85c8c6754c0c64348000ffea23ace870efcad60
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-05 20:53:54 -0300

    feat: add raised button component

---
+++ frontend/src/widgets/RaisedButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="raised__button">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['text']
+}
+</script>
+
+<style scoped>
+  .raised__button {
+    background-color: #FFBC43;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 500;
+    border: none;
+    height: 60px;
+    width: 100%;
+    max-width: 374px;
+    border-radius: 8px;
+    outline: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  }
+  .raised__button:hover {
+    opacity: 0.8;
+  }
+</style>
