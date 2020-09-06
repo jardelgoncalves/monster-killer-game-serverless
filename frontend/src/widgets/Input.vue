085d1e9b86ae21b22ea5faf674f815e955c90775
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-05 22:22:04 -0300

    feat: add input component

---
+++ frontend/src/widgets/Input.vue
@@ -0,0 +1,30 @@
+<template>
+  <input class="input" :placeholder="placeholder" />
+</template>
+
+<script>
+export default {
+  props: ['placeholder']
+}
+</script>
+
+<style scoped>
+  .input {
+    background-color: #2F3578;
+    color: #FFBC43;
+    font-size: 18px;
+    font-weight: 600;
+    border: none;
+    height: 60px;
+    width: 100%;
+    max-width: 374px;
+    border-radius: 8px;
+    display: flex;
+    align-items: center;
+    padding: 0 8px;
+  }
+  .input::placeholder {
+    color: #7175A2;
+    font-weight: 400;
+  }
+</style>
