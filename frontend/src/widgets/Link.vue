80c1a00dd607ded7c2d53e17ba9e9a42b7defbf2
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-06 00:09:49 -0300

    feat: add link component

---
+++ frontend/src/widgets/Link.vue
@@ -0,0 +1,22 @@
+<template>
+  <a class="link" :href="path">{{ text }}</a>
+</template>
+
+<script>
+export default {
+  props: ['path', 'text']
+}
+</script>
+
+<style scoped>
+  .link {
+    color: #FFBC43;
+    font-size: 18px;
+    font-weight: 500;
+    cursor: pointer;
+    padding: 5px;
+  }
+  .link:hover {
+    opacity: 0.8;
+  }
+</style>
