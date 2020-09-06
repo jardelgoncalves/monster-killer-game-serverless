c678c84250526a9156bf207cb9e325eaf2c95bfc
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-06 14:28:22 -0300

    feat: add link to ranking

--- frontend/src/views/ViewRoot.vue
+++ frontend/src/views/ViewRoot.vue
@@ -4,18 +4,21 @@
       <app-logo />
       <h1>Monster Killer</h1>
       <app-raised-button text="Get Start"></app-raised-button>
+      <app-link text="Ranking" path="/ranking" class="view-link" />
     </div>
   </app-layout>
 </template>
 
 <script>
   import RaisedButton from '@/widgets/RaisedButton.vue';
+  import Link from '@/widgets/Link.vue';
   import Layout from '@/templates/Layout.vue';
   import Logo from '@/components/Logo.vue';
 
   export default {
     components: {
       'app-layout': Layout,
+      'app-link': Link,
       'app-raised-button': RaisedButton,
       'app-logo': Logo,
     }
@@ -38,5 +41,9 @@
     line-height: 49px;
     margin-top: 14px;
     margin-bottom: 32px;
+  }
+
+  .root .view-link {
+    margin-top: 12px;
   }
 </style>
