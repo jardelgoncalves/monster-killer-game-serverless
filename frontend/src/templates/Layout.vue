8e29f8994129c3822882b779f367b6f5ade491f4
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-06 13:28:07 -0300

    feat: add Layout component

---
+++ frontend/src/templates/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="layout" :class="{layout__center: center}">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["center"]
+}
+</script>
+
+<style>
+  body {
+    background-image: url("../assets/bg.png");
+    background-repeat: no-repeat;
+    background-size: 100%;
+    background-attachment: fixed;
+    
+  }
+
+  .layout {
+    display: flex;
+    flex-direction: column;
+  } 
+
+  .layout__center {
+    align-items: center;
+    justify-content: center;
+  }
+</style>
