57c34cf5fcac49ec5ff599028a38aa610d4de560
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-06 16:51:40 -0300

    refactor: add margin

--- frontend/src/components/Panel.vue
+++ frontend/src/components/Panel.vue
@@ -14,7 +14,7 @@ export default {
     background: #171E69;
     box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
     border-radius: 8px;
-    margin: 10px;
+    margin: 8px;
     padding: 20px;
     flex: 1;
   }
