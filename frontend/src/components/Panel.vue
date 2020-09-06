1ed57f064ca0d8b078b31fbbad5b515cea8ca9ef
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-06 01:09:55 -0300

    feat: add panel component

---
+++ frontend/src/components/Panel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="panel">    
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .panel {
+    background: #171E69;
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+    margin: 10px;
+    padding: 20px;
+    flex: 1;
+  }
+</style>
