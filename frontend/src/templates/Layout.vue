0d0e5b9296bd6934f0174360d43e42cbdc1a2dc2
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-06 16:51:54 -0300

    refactor: add scoped

--- frontend/src/templates/Layout.vue
+++ frontend/src/templates/Layout.vue
@@ -10,16 +10,14 @@ export default {
 }
 </script>
 
-<style>
-  body {
+<style scoped>
+
+  .layout {
     background-image: url("../assets/bg.png");
     background-repeat: no-repeat;
     background-size: cover;
     background-attachment: fixed;
     background-position: center top;
-  }
-
-  .layout {
     display: flex;
     flex-direction: column;
     height: 100%;
