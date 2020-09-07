df72471707c7d617d2967891220376bf217abb28
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-07 13:03:19 -0300

    feat: add middleware to validation

---
+++ api/src/util/decoratorValidator.js
@@ -0,0 +1,18 @@
+const decoratorValidator = (fn, schema, argType) => {
+  return async function(event) {
+    const data = JSON.parse(event[argType]);
+    const { error, value } = schema.validate(data, { abortEarly: true });
+
+
+    event[argType] = value
+
+    if(!error) return fn.apply(this, arguments);
+
+    return  {
+      statusCode: 422,
+      body: error.message
+    }
+  }
+}
+
+module.exports = decoratorValidator
