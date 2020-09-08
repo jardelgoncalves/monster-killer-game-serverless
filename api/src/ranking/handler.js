7c179c14283d2c9e0b87800c7a09587b3bc34122
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-07 23:05:32 -0300

    refactor: add treat event

--- api/src/ranking/handler.js
+++ api/src/ranking/handler.js
@@ -1,10 +1,60 @@
-const handler = {
+const uuid = require('uuid').v4
+const response = require('../util/response');
+
+class Handler {
+  constructor({ dynamoDbService }) {
+    this.dynamoDbService = dynamoDbService;
+    this.dynamoRankingTable = process.env.DYNAMO_RANKING_TABLE
+  }
+
+  treatRecords({ Records }) {
+    if(!Records || !Array.isArray(Records)) return [];
+
+    return Records.map(({ dynamodb }) => this.prepareData({
+      userId: dynamodb.NewImage.userId.S,
+      username: dynamodb.NewImage.username.S,
+      score: dynamodb.NewImage.score.N,
+    }))
+  }
+
+  prepareData(data) {
+    const params = {
+      TableName: this.dynamoRankingTable,
+      Item: {
+        ...data,
+        id: uuid(),
+        createdAt: new Date().toISOString()
+      }
+    }
+    return params
+  }
+
+  
+  async insert(data) {
+    return this.dynamoDbService.put(data).promise();
+  }
+
   async main(event) {
-    console.log('Event:', JSON.stringify(event, null, 2));
-    return {
-      statusCode: 200
+    try {
+      const data = this.treatRecords(event)
+
+      await Promise.all(
+        data.map(d => this.insert(d))
+      )
+      
+      return response.success({ success: true })
+    } catch (error) {
+      console.error('Deu ruim**', error.stack)
+      return response.error({ statusCode: 500 })
     }
   }
 }
 
-module.exports = handler.main.bind(handler);
+const AWS = require('aws-sdk');
+const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
+const handler = new Handler({
+  dynamoDbService: DynamoDB
+});
+
+module.exports = handler.main.bind(handler)
