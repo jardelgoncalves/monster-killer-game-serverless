6b4904245a1d4306469153a02dd5f80c8c1b610b
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-08 01:04:38 -0300

    feat: add integration with api

--- frontend/src/views/StartGame.vue
+++ frontend/src/views/StartGame.vue
@@ -2,8 +2,16 @@
   <app-layout center="true">
     <div class="root">
       <h1>We need the warrior's name</h1>
-      <app-input placeholder="Your name" />
-      <app-raised-button text="Start Game"></app-raised-button>
+      <app-input
+        :value="username"
+        :change="onInputChange"
+        name="username"
+        placeholder="Your name"
+      />
+      <app-raised-button
+        text="Start Game"
+        :click="fetch"
+      />
     </div>
   </app-layout>
 </template>
@@ -13,11 +21,40 @@
   import Input from '@/widgets/Input.vue';
   import Layout from '@/templates/Layout.vue';
 
+  import api from '../services/api'
+  import constant from '../utils/constant'
+
   export default {
     components: {
       'app-layout': Layout,
       'app-raised-button': RaisedButton,
       'app-input': Input,
+    },
+    data() {
+      return {
+        username: ''
+      }
+    },
+    methods: {
+      onInputChange(e) {
+        const { name, value } = e.target
+        this[name] = value;
+      },
+      async fetch() {
+        const user = { username: this.username };
+        try {
+          const { data } = await api.post('user', user);
+          localStorage.setItem(constant.TOKEN, JSON.stringify(data))
+          this.$router.push('/game')
+        } catch(err) {
+          console.log(err)
+        }
+      }
+    },
+    beforeCreate() {
+      if (localStorage.getItem(constant.TOKEN)) {
+        this.$router.push('/game')
+      }
     }
   }
 </script>
