7bda0bd3b406840e3c0dbb225a93d8973b5cd418
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-09 00:43:09 -0300

    feat: add icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faArrowLeft)
 
 Vue.config.productionTip = false
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
