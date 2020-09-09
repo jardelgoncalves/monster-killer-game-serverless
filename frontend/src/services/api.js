c7e28f04f5f52effbe7a168ee33ae01fb81c43ec
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-09 00:42:32 -0300

    feat: add url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://jaowxmg315.execute-api.us-east-1.amazonaws.com/dev/'
+  baseURL: 'https://2n88vpooki.execute-api.us-east-1.amazonaws.com/dev/'
 })
 
 export default api
