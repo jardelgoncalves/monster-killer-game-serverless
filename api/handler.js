ef7ada06eb541d969c13fae83db454f2748a028b
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-11 00:48:10 -0300

    refactor: new struture for functions handlers

--- api/handler.js
+++ api/handler.js
@@ -1,7 +1,26 @@
-'use strict';
+const AWS = require('aws-sdk');
 
-module.exports = {
-  ...require('./src/score'),
-  ...require('./src/ranking'),
-  ...require('./src/user'),
-}
+const ServiceManager = require('./src/services');
+const globalEnum = require('./src/util/globalEnum');
+const decoratorValidator = require('./src/util/decoratorValidator');
+const { userSchema, scoreSchema } = require('./src/schemas');
+
+const DynamoDB = new AWS.DynamoDB.DocumentClient();
+const serviceManager = new ServiceManager({ dynamoDbService: DynamoDB })
+
+const { userService, scoreService, rankingService } = serviceManager;
+
+exports.createUser = decoratorValidator(
+  userService.create.bind(userService),
+  userSchema,
+  globalEnum.ARG_TYPE.BODY
+);
+
+exports.createScore = decoratorValidator(
+  scoreService.create.bind(scoreService),
+  scoreSchema,
+  globalEnum.ARG_TYPE.BODY
+);
+
+exports.rankingGetAll = rankingService.findAll.bind(rankingService)
+exports.rankingHandlerEvent = rankingService.treatEvent.bind(rankingService)
